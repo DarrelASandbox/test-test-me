83ea958aabdc0c5bddd46005b7b5f7f5d6b0e6f5
DarrelA <darrelaiscoding@gmail.com>
2022-03-28 23:00:40 +0800

    Added new test in samplewebproject & discover bug involving tests that do not require asynchronous operation in sampleproject

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -7,7 +7,7 @@ it('has a text input', async () => {
   assert(input);
 });
 
-it('shows a success message with a valid', async () => {
+it('shows a success message with a valid email', async () => {
   const dom = await render('index.html');
   const input = dom.window.document.querySelector('input');
   input.value = 'chicken@factory.com';
@@ -16,7 +16,19 @@ it('shows a success message with a valid', async () => {
     .dispatchEvent(new dom.window.Event('submit'));
 
   const h1 = dom.window.document.querySelector('h1');
-  assert.strictEqual(h1.innerHTML, 'Looks good!!');
+  assert.strictEqual(h1.innerHTML, 'Looks good!');
+});
+
+it('shows a failed message with a invalid email', async () => {
+  const dom = await render('index.html');
+  const input = dom.window.document.querySelector('input');
+  input.value = 'chickenfactory.com';
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const h1 = dom.window.document.querySelector('h1');
+  assert.strictEqual(h1.innerHTML, 'Invalid email');
 });
 
 /*               <<<<<<<< Script execution delay issue >>>>>>>>
